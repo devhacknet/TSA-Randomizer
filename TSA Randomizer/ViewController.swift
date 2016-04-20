f217572400736572b1f7acf85893a53d65c80a18
Dat Nguyen Thanh <dat.nguyen@axonactive.vn>
2016-04-20 15:16:22 +0700

    first commit

--- TSA Randomizer/ViewController.swift
+++ TSA Randomizer/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgLogo: UIImageView!
+    @IBOutlet weak var imgArrow: UIImageView!
+    var isReady: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+   
+    @IBAction func showDirection(sender: AnyObject) {
+        if(!isReady) {
+            return
+        }
+        
+        if(randomBool()) {
+            imgArrow.image = UIImage(named: "left")
+            print("left")
+        } else {
+             imgArrow.image = UIImage(named: "right")
+            print("right")
+        }
+        changeStatus()
+        
+        let time = dispatch_time(dispatch_time_t(DISPATCH_TIME_NOW), 1 * Int64(NSEC_PER_SEC))
+        dispatch_after(time, dispatch_get_main_queue()) {
+            self.changeStatus()
+        }
+    }
+    
+    func randomBool() -> Bool {
+        return arc4random_uniform(2) == 0 ? true: false
+    }
+    
+    func changeStatus() {
+        isReady = !isReady
+        imgArrow.hidden = isReady
+        imgLogo.hidden = !isReady
+    }
+    
 }
 
